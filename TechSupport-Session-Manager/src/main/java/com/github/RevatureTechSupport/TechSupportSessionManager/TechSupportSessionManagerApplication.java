19390889583e7a035d42a67b70b7333b7ec7ed91
Jonathan Gomez <officialjonathangomez@hotmail.com>
2021-09-15 11:13:43 -0500

    Init project

---
+++ TechSupport-Session-Manager/src/main/java/com/github/RevatureTechSupport/TechSupportSessionManager/TechSupportSessionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.RevatureTechSupport.TechSupportSessionManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSupportSessionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSupportSessionManagerApplication.class, args);
+	}
+
+}
