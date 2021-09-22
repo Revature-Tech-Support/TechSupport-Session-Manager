7ca2c557935a585920c5d9a6560ada2de7154166
Olenka Quispe <40580430+Olenkaqh@users.noreply.github.com>
2021-09-22 18:37:06 -0400

    Update TechSupportSessionManagerApplication.java

--- TechSupport-Session-Manager/src/main/java/com/github/RevatureTechSupport/TechSupportSessionManager/TechSupportSessionManagerApplication.java
+++ TechSupport-Session-Manager/src/main/java/com/github/RevatureTechSupport/TechSupportSessionManager/TechSupportSessionManagerApplication.java
@@ -2,6 +2,7 @@ package com.github.RevatureTechSupport.TechSupportSessionManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
