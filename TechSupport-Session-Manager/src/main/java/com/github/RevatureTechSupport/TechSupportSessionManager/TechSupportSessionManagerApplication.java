4c336548cc5b7225cc7e498eeeedd6f073fbbae3
JonathanAGomez <47328753+JonathanAGomez@users.noreply.github.com>
2021-09-22 10:26:35 -0500

    Conflict Markers removed
    conflict markers were present in the code. conflict was resolved and markers removed.

--- TechSupport-Session-Manager/src/main/java/com/github/RevatureTechSupport/TechSupportSessionManager/TechSupportSessionManagerApplication.java
+++ TechSupport-Session-Manager/src/main/java/com/github/RevatureTechSupport/TechSupportSessionManager/TechSupportSessionManagerApplication.java
@@ -9,9 +9,4 @@ public class TechSupportSessionManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TechSupportSessionManagerApplication.class, args);
     }
-<<<<<<< HEAD
-
 }
-=======
-}
->>>>>>> b1b0184a004b34250e198abd5b072c10ae1fe42b
